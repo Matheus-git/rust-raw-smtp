9485094830c392d734af091a56beba342a0baa8b
Matheus-git <mathiew0@gmail.com>
2025-03-16 12:29:08 -0300

    fix: Removing default text and making the messages bold.

--- src/controller/email_input.rs
+++ src/controller/email_input.rs
@@ -1,6 +1,5 @@
 use std::io::{self, Write};
 
-#[derive(Debug)]
 pub struct EmailInput {
     pub from: String,
     pub to: String,
@@ -10,48 +9,27 @@ pub struct EmailInput {
 
 impl EmailInput {
     pub fn new() -> Self {
-        let default_from = "sender@example.com".to_string();
-        let default_to = "recipient@example.com".to_string();
-        let default_data = "This is a test email.".to_string();
-        let default_subject = "Test Email".to_string();
-
-        let from = read_input(
-            &format!("Enter the sender's address (default: {}): ", default_from),
-            default_from,
-        );
-        let to = read_input(
-            &format!("Enter the recipient's address (default: {}): ", default_to),
-            default_to,
-        );
-        let subject = read_input(
-            &format!("Enter the subject (default: {}): ", default_subject),
-            default_subject,
-        );
-        let data = read_input(
-            &format!("Enter the email body (default: {}): ", default_data),
-            default_data,
-        );
+        let from = read_input("Enter the sender's address:");
+        let to = read_input("Enter the recipient's address:");
+        let subject = read_input("Enter the subject:");
+        let data = read_input("Enter the email body:");
 
         EmailInput {
             from,
             to,
-            data,
             subject,
+            data,
         }
     }
 }
 
-fn read_input(prompt: &str, default: String) -> String {
-    print!("{}", prompt);
+fn read_input(prompt: &str) -> String {
+    print!("\x1b[1m{} \x1b[0m", prompt);
     io::stdout().flush().unwrap();
 
     let mut input = String::new();
     io::stdin().read_line(&mut input).expect("Failed to read input");
 
     let input = input.trim();
-    if input.is_empty() {
-        default
-    } else {
-        input.to_string()
-    }
+    input.to_string()
 }
